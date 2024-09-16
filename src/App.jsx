de4b1f775dbdc027bb54a1ea970387fb3ddc8886
DerekG29 <derekgrubbs29@gmail.com>
2024-09-15 22:04:15 -0400

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState('00:00:00');
+  const [on, setOn] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      const t = new Date();
+      let h = t.getHours();
+      let m = t.getMinutes();
+      let s = t.getSeconds();
+      setTime(`${h}:${m}:${s}`);
+    }, 1000)
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World</h1>
+      <button onClick={() => setOn(!on)}>on/off</button>
+      <div className={on ? 'dot-on' : 'dot-off'}></div>
+      <h2>{time}</h2>
     </>
   )
 }
